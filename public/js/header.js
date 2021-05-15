467102dd857e7f4c081fecdd5fa98073514382ea
Aaron Robb <aaron@codecademy.com>
2021-05-15 03:15:28 -0400

    Restructure

--- public/js/header.js
+++ public/js/header.js
@@ -1,16 +1,19 @@
+/** @format */
+
 const body = document.body;
-let isHeaderSticky=false;
- 
+let isHeaderSticky = false;
+
 window.addEventListener("scroll", () => {
-	if (!isHeaderSticky){
-		if (window.pageYOffset > 600){
-			isHeaderSticky = true;
-			document.querySelector("header").classList.add('revealed');
-		}
-	} else {
-		if (window.pageYOffset < 599){
-			isHeaderSticky = false;
-			document.querySelector("header").classList.remove('revealed');
-		}
-	}
+  if (!isHeaderSticky) {
+    if (window.pageYOffset > 600) {
+      isHeaderSticky = true;
+      console.log(document.querySelector(".header"));
+      document.querySelector(".header").classList.add("header__revealed");
+    }
+  } else {
+    if (window.pageYOffset < 599) {
+      isHeaderSticky = false;
+      document.querySelector(".header").classList.remove("header__revealed");
+    }
+  }
 });
