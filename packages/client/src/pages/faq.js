a66d44c5cd38d838b561a4a1f9c529978ed9dd35
Sonny Li <sonnynomnom@gmail.com>
2021-06-21 21:59:18 -0400

    Add Accordion for faq.js

--- packages/client/src/pages/faq.js
+++ packages/client/src/pages/faq.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Header } from '../components/Header';
 import { Footer } from '../components/Footer';
 import { GithubLogo } from '../components/GithubLogo';
+
+const Accordion = ({ title, children }) => {
+  const [expand, setExpand] = useState(false);
+
+  return (
+    <div className="collapse-area">
+      <button
+        type="button"
+        className="collapse-button collapsible"
+        onClick={() => setExpand(!expand)}
+      >
+        {title}
+      </button>
+      {expand && <div className="collapse-content content">{children}</div>}
+    </div>
+  );
+};
 
 export function FAQ() {
   return (
@@ -40,7 +57,6 @@ export function FAQ() {
 
         .content {
           padding: 1rem;
-          display: none;
           overflow: hidden;
           background-color: var(--color-cloud-nine);
         }
@@ -61,142 +77,116 @@ export function FAQ() {
       <main className="layout layout__small layout-grid layout-grid__sm">
         <h1 className="page-subtitle">💬 Frequently Asked Questions</h1>
         <section className="layout-grid layout-grid__sm collapse">
-          <div className="collapse-area">
-            <button type="button" className="collapse-button collapsible">
-              Do I need a microphone for Karaoke Nite?
-            </button>
+          <Accordion title="Do I need a microphone for Karaoke Nite?">
+            <p>
+              While you don't need a handheld microphone for Karaoke Nite, it
+              certainly makes the experience better!
+            </p>
 
-            <div className="collapse-content content">
-              <p>
-                While you don't need a handheld microphone for Karaoke Nite, it
-                certainly makes the experience better!
-              </p>
+            <p>Here are some microphones that we recommend:</p>
 
-              <p>Here are some microphones that we recommend:</p>
+            <ul className="list">
+              <li>
+                $49.99
+                <a
+                  className="link"
+                  href="https://www.amazon.com/Singing-Machine-Bluetooth-Microphone-CPK545/dp/B07SLFVGSC/"
+                  target="_blank"
+                >
+                  Carpool Karaoke Mic
+                </a>
+              </li>
+              <li>
+                $16.99
+                <a
+                  className="link"
+                  href="https://www.amazon.com/Handheld-Microphone-Nintendo-Singing-Compatible/dp/B08K6WXR29"
+                  target="_blank"
+                >
+                  USB Handheld Microphone
+                </a>
+              </li>
+            </ul>
+            <p>
+              P.S. We are working on a new and innovative way to sing without
+              using a professional microphone (*cough QR code and mobile), keep
+              on the look out for a feature update!
+            </p>
+          </Accordion>
+          <Accordion title="How do I connect my microphone to the PC?">
+            <p>You can do this in three simple steps:</p>
+            <ol className="list">
+              <li>Charge the microphone unit or make sure there's battery.</li>
+              <li>
+                Plug the other end of the cable into computer USB port or AUX
+                port.
+              </li>
+              <li>
+                Go to audio settings on your computer and set as input device.
+              </li>
+            </ol>
 
-              <ul className="list">
-                <li>
-                  $49.99
-                  <a
-                    className="link"
-                    href="https://www.amazon.com/Singing-Machine-Bluetooth-Microphone-CPK545/dp/B07SLFVGSC/"
-                    target="_blank"
-                  >
-                    Carpool Karaoke Mic
-                  </a>
-                </li>
-                <li>
-                  $16.99
-                  <a
-                    className="link"
-                    href="https://www.amazon.com/Handheld-Microphone-Nintendo-Singing-Compatible/dp/B08K6WXR29"
-                    target="_blank"
-                  >
-                    USB Handheld Microphone
-                  </a>
-                </li>
-              </ul>
-              <p>
-                P.S. We are working on a new and innovative way to sing without
-                using a professional microphone (*cough QR code and mobile),
-                keep on the look out for a feature update!
-              </p>
-            </div>
-          </div>
-          <div className="collapse-area">
-            <button type="button" className="collapse-button collapsible">
-              How do I connect my microphone to the PC?
-            </button>
-            <div className="collapse-content content">
-              <p>You can do this in three simple steps:</p>
-              <ol className="list">
-                <li>
-                  Charge the microphone unit or make sure there's battery.
-                </li>
-                <li>
-                  Plug the other end of the cable into computer USB port or AUX
-                  port.
-                </li>
-                <li>
-                  Go to audio settings on your computer and set as input device.
-                </li>
-              </ol>
+            <p>
+              <b>Please note:</b>
+            </p>
 
-              <p>
-                <b>Please note:</b>
-              </p>
+            <p>
+              Due to Covid-19, we discourage all users from sharing microphones
+              with friends & hope everyone adopt a good microphone hygiene
+              routine!
+            </p>
+          </Accordion>
+          <Accordion title="How do I hear myself through the speakers?">
+            <p>Windows users:</p>
+            <ol className="list">
+              <li>Click on "Spearkers" in the taskbar.</li>
+              <li>
+                Click on "Sound" and make sure your microphone is set as the
+                default.
+              </li>
+              <li>Click on "Recording".</li>
+              <li>Check the "Listen to this device" box.</li>
+            </ol>
+            <p>Mac users:</p>
+            <ol className="list">
+              <li>Open QuickTime Player.</li>
+              <li>Click "File" ➡ "New Audio Recording".</li>
+              <li>
+                Move the volume bar higher and higher until you can hear
+                yourself.
+              </li>
+            </ol>
+          </Accordion>
+          <Accordion title="What are the new updates?">
+            <h3>v0.2 is currently in production! Scoped to launch in May.</h3>
+            <h4>Release Build v0.1.3</h4>
+            <ul className="list list__dense">
+              <li>🆕 Added a /faq page.</li>
+              <li>🏗 Updated /about page.</li>
+              <li>🏗 Cleaned up the codebase in preparation of v0.2.</li>
+              <li>💸 Compiled the pre-seed pitch deck.</li>
+            </ul>
 
-              <p>
-                Due to Covid-19, we discourage all users from sharing
-                microphones with friends & hope everyone adopt a good microphone
-                hygiene routine!
-              </p>
-            </div>
-          </div>
-          <div className="collapse-area">
-            <button type="button" className="collapse-button collapsible">
-              How do I hear myself through the speakers?
-            </button>
+            <h4>Release Build v0.1.2</h4>
+            <ul className="list list__dense">
+              <li>🆕 Added a Privacy Policy. Thanks, Min-Kyu!</li>
+              <li>🆕 Added demo video.</li>
+              <li>🆕 Added release notes.</li>
+              <li>🆕 Added blog posts.</li>
+              <li>🏗 Removed Patreon links.</li>
+              <li>🐛 Fixed the "Let's Get It On" autoplay bug.</li>
+            </ul>
 
-            <div className="collapse-content content">
-              <p>Windows users:</p>
-              <ol className="list">
-                <li>Click on "Spearkers" in the taskbar.</li>
-                <li>
-                  Click on "Sound" and make sure your microphone is set as the
-                  default.
-                </li>
-                <li>Click on "Recording".</li>
-                <li>Check the "Listen to this device" box.</li>
-              </ol>
-
-              <p>Mac users:</p>
-              <ol className="list">
-                <li>Open QuickTime Player.</li>
-                <li>Click "File" ➡ "New Audio Recording".</li>
-                <li>
-                  Move the volume bar higher and higher until you can hear
-                  yourself.
-                </li>
-              </ol>
-            </div>
-          </div>
-          <div className="collapse-area">
-            <button type="button" className="collapsible collapse-button">
-              What are the new updates?
-            </button>
-
-            <div className="content collapse-content">
-              <h3>v0.2 is currently in production! Scoped to launch in May.</h3>
-              <h4>Release Build v0.1.3</h4>
-              <ul className="list list__dense">
-                <li>🆕 Added a /faq page.</li>
-                <li>🏗 Updated /about page.</li>
-                <li>🏗 Cleaned up the codebase in preparation of v0.2.</li>
-                <li>💸 Compiled the pre-seed pitch deck.</li>
-              </ul>
-
-              <h4>Release Build v0.1.2</h4>
-              <ul className="list list__dense">
-                <li>🆕 Added a Privacy Policy. Thanks, Min-Kyu!</li>
-                <li>🆕 Added demo video.</li>
-                <li>🆕 Added release notes.</li>
-                <li>🆕 Added blog posts.</li>
-                <li>🏗 Removed Patreon links.</li>
-                <li>🐛 Fixed the "Let's Get It On" autoplay bug.</li>
-              </ul>
-
-              <h4>Release Build v0.1.1</h4>
-              <ul className="list list__dense">
-                <li>
-                  🆕 Added floating animation to the stars on the landing page.
-                </li>
-                <li>🆕 Added hover effects to anchor links.</li>
-                <li>🆕 Added a /blog page.</li>
-                <li>🐛 Fixed social channel links bug in the footer.</li>
-              </ul>
-            </div>
-          </div>
+            <h4>Release Build v0.1.1</h4>
+            <ul className="list list__dense">
+              <li>
+                🆕 Added floating animation to the stars on the landing page.
+              </li>
+              <li>🆕 Added hover effects to anchor links.</li>
+              <li>🆕 Added a /blog page.</li>
+              <li>🐛 Fixed social channel links bug in the footer.</li>
+            </ul>
+          </Accordion>
         </section>
         <section>
           <p>
