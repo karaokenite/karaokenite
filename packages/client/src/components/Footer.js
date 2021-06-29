1a320906e504b481f99cc929f2f5ce036faa5528
Sonny Li <sonnynomnom@gmail.com>
2021-06-29 00:48:53 -0400

    Footer icons using .map()

--- packages/client/src/components/Footer.js
+++ packages/client/src/components/Footer.js
@@ -1,5 +1,38 @@
 import React from 'react';
 
+const Icons = ({ href, src }) => {
+  return (
+    <div className="column">
+      <a href={href} target="_blank">
+        <img className="social" src={src} />
+      </a>
+    </div>
+  );
+};
+
+const icons = [
+  {
+    href: 'https://www.facebook.com/karaokenite2021',
+    src: 'https://cdn.glitch.com/b316bbdc-0b0c-4c6d-94fb-fffb37f510a9%2Ffacebook-logo.svg?v=1597727187930',
+  },
+  {
+    href: 'https://www.instagram.com/karaokenite',
+    src: 'https://cdn.glitch.com/b316bbdc-0b0c-4c6d-94fb-fffb37f510a9%2Finstagram-logo.svg?v=1597727187949',
+  },
+  {
+    href: 'https://www.twitter.com/karaoke_nite',
+    src: 'https://cdn.glitch.com/b316bbdc-0b0c-4c6d-94fb-fffb37f510a9%2Ftwitter-logo.svg?v=1597727187978',
+  },
+  {
+    href: 'https://www.github.com/karaokenite',
+    src: 'https://cdn.glitch.com/b316bbdc-0b0c-4c6d-94fb-fffb37f510a9%2Fgithub-logo.svg?v=1597727187892',
+  },
+  {
+    href: 'https://discord.gg/xUc8m25',
+    src: 'https://cdn.glitch.com/b316bbdc-0b0c-4c6d-94fb-fffb37f510a9%2Fdiscord%20logo.svg?v=1599600184242',
+  },
+];
+
 export const Footer = () => {
   return (
     <footer className="footer">
@@ -16,50 +49,9 @@ export const Footer = () => {
         </div>
 
         <div className="footer-icons">
-          <div className="column">
-            <a href="https://www.facebook.com/karaokenite2021" target="_blank">
-              <img
-                className="social"
-                src="https://cdn.glitch.com/b316bbdc-0b0c-4c6d-94fb-fffb37f510a9%2Ffacebook-logo.svg?v=1597727187930"
-              />
-            </a>
-          </div>
-
-          <div className="column">
-            <a href="https://www.instagram.com/karaokenite" target="_blank">
-              <img
-                className="social"
-                src="https://cdn.glitch.com/b316bbdc-0b0c-4c6d-94fb-fffb37f510a9%2Finstagram-logo.svg?v=1597727187949"
-              />
-            </a>
-          </div>
-
-          <div className="column">
-            <a href="https://www.twitter.com/karaoke_nite" target="_blank">
-              <img
-                className="social"
-                src="https://cdn.glitch.com/b316bbdc-0b0c-4c6d-94fb-fffb37f510a9%2Ftwitter-logo.svg?v=1597727187978"
-              />
-            </a>
-          </div>
-
-          <div className="column">
-            <a href="https://www.github.com/karaokenite" target="_blank">
-              <img
-                className="social"
-                src="https://cdn.glitch.com/b316bbdc-0b0c-4c6d-94fb-fffb37f510a9%2Fgithub-logo.svg?v=1597727187892"
-              />
-            </a>
-          </div>
-
-          <div className="column">
-            <a href="https://discord.gg/xUc8m25" target="_blank">
-              <img
-                className="social"
-                src="https://cdn.glitch.com/b316bbdc-0b0c-4c6d-94fb-fffb37f510a9%2Fdiscord%20logo.svg?v=1599600184242"
-              />
-            </a>
-          </div>
+          {icons.map((icon) => (
+            <Icons href={icon.href} src={icon.src} />
+          ))}
         </div>
       </div>
     </footer>
